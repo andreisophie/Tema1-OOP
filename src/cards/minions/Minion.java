2648f303dff5fc6bf773dd329f8bfe5537040e4e
andreisophie <andreisophie@yahoo.com>
2022-11-12 13:30:43 +0200

    Added minion interfaces and abstract classes

---
+++ src/cards/minions/Minion.java
@@ -0,0 +1,72 @@
+package cards.minions;
+
+import java.util.Arrays;
+
+public abstract class Minion {
+    private int health;
+    private int mana;
+    private int attackDamage;
+    private String Description;
+    private String[] colors;
+    private String name;
+
+    public int getHealth() {
+        return health;
+    }
+
+    public void setHealth(int health) {
+        this.health = health;
+    }
+
+    public int getMana() {
+        return mana;
+    }
+
+    public void setMana(int mana) {
+        this.mana = mana;
+    }
+
+    public int getAttackDamage() {
+        return attackDamage;
+    }
+
+    public void setAttackDamage(int attackDamage) {
+        this.attackDamage = attackDamage;
+    }
+
+    public String getDescription() {
+        return Description;
+    }
+
+    public void setDescription(String description) {
+        Description = description;
+    }
+
+    public String[] getColors() {
+        return colors;
+    }
+
+    public void setColors(String[] colors) {
+        this.colors = colors;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    @Override
+    public String toString() {
+        return "Minion{" +
+                "health=" + health +
+                ", mana=" + mana +
+                ", attackDamage=" + attackDamage +
+                ", Description='" + Description + '\'' +
+                ", colors=" + Arrays.toString(colors) +
+                ", name='" + name + '\'' +
+                '}';
+    }
+}
---
+++ src/cards/minions/PremiumMinion.java
@@ -0,0 +1,5 @@
+package cards.minions;
+
+public interface PremiumMinion {
+    void ability(Minion target);
+}
